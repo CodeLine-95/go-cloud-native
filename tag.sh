7a3370b41cb07109d5fb201f871ba8e06e94dd92
p-qiaoshuai <p-qiaoshuai@xiaomi.com>
2023-09-15 22:55:27 +0800

    feat: 项目初始化

--- tag.sh
+++ tag.sh
@@ -2,26 +2,17 @@
 ###
  # @Author: CodeLine
  # @Date: 2023-09-15 22:48:13
- # @LastEditors: CodeLine
- # @LastEditTime: 2023-09-15 22:51:51
+ # @LastEditors: p-qiaoshuai p-qiaoshuai@xiaomi.com
+ # @LastEditTime: 2023-09-15 22:54:47
  # @FilePath: /go-cloud-native/tag.sh
 ### 
 
-prefix="test-"
 pre_prefix="pre-"
-
-if `git status | grep "RELEASE" &>/dev/null`; then
-    prefix="pro-"
-fi
-
-if [ $1 ] && [ $1 = ${pre_prefix} ]; then
-    prefix=${pre_prefix}
-fi
 
 function mi-tag() {
     git push
     git pull --tags
-    local new_tag=$(echo ${prefix}$(date +'%Y%m%d')-$(git tag -l "${prefix}$(date +'%Y%m%d')-*" | wc -l | xargs printf '%02d'))
+    local new_tag=$(echo ${pre_prefix}$(date +'%Y%m%d')-$(git tag -l "${pre_prefix}$(date +'%Y%m%d')-*" | wc -l | xargs printf '%02d'))
     echo ${new_tag}
     git tag ${new_tag}
     git push origin $new_tag
